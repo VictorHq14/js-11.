ea5b6b600b75d6c6880f0b938f734b568e61bc5a
VictorHq14 <victorhugo.ce0212@gmail.com>
2024-07-15 18:09:23 -0300

    alua 11

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const arr = ['a','b','c','d','e','f']
+
+const newArr2 = arr.map(elemento => elemento.toUpperCase())
+console.log(newArr2)
+
+// const numeros = [5, 10, 25, 20];
+
+// const soma = numeros.reduce ((acumulador,numero) => acumulador + numero, 40);
+
+// console.log("Array original:", numeros);
+// console.log("Soma dos numeros:", soma);
+
+// const numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+// const xnumeros = numeros.map( numero => numero ** 2);
+
+// console.log(xnumeros)
